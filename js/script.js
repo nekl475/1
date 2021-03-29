7c159d357ddb9b294f331e502ee91770bcd2015f
Kate <nekl47511@gmail.com>
2021-03-29 23:25:24 +0300

    adaptiveheigth false

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
         responsive: [{
